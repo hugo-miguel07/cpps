ee1abfbd7e9788e5fc7a53a1de5501a43c4f10b3
Hugo Miguel Tavares Cacao <htavares@c2r2s5.42lisboa.com>
2026-04-13 17:55:10 +0100

    starting cpp1

---
+++ cpp1/ex00/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htavares <htavares@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 17:45:55 by htavares          #+#    #+#             */
+/*   Updated: 2026/04/13 17:51:30 by htavares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+
+class Zombie
+{
+	private:
+	std::string name;
+	
+	public:
+	void announce(void);
+};
+
+#endif
